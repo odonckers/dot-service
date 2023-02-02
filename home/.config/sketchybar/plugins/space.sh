597b896dbbda6c3eb171d2fe7c2da41b9990b264
Owen Donckers <odonckers.dev@proton.me>
2023-02-02 08:32:44 -0500

    Made more stuff change that looks better

--- home/.config/sketchybar/plugins/space.sh
+++ home/.config/sketchybar/plugins/space.sh
@@ -4,10 +4,10 @@
 # the space invoking this script (with name: $NAME) is currently selected:
 # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
 #
-  sketchybar --set $NAME label.highlight=$SELECTED
+  sketchybar --set $NAME icon.highlight=$SELECTED
 
 if [ "$SELECTED" = true ]; then
-  sketchybar --set $NAME label="􀝜"
+  sketchybar --set $NAME icon="􀝜"
 else
-  sketchybar --set $NAME label="􀀀"
+  sketchybar --set $NAME icon="􀀀"
 fi
